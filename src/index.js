115163fab3962f3c484c213fa049c0e37df016b7
Bloodsucker <ab71511@gmail.com>
2023-04-06 16:57:57 -0700

    plugin registeration fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const createTesla = require("./createTesla")
 module.exports = function register(homebridge) {
   const Service = homebridge.hap.Service
   const Characteristic = homebridge.hap.Characteristic
-  homebridge.registerAccessory('homebridge-tesla-bloodsucker', 'Tesla', createTesla({
+  homebridge.registerAccessory('homebridge-bloody-tesla', 'Tesla', createTesla({
     Service,
     Characteristic,
   }));
